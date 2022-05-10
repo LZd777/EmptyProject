0c86c868565b570bb376f98c90aa6f9895b7bd09
liuzhidan <liuzhidan@163.com>
2022-05-10 13:44:36 +0800

    String

---
+++ day15/src/Test02.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+public class Test02 {
+    public static void main(String[] args) {
+        String s1 = "abd423abc3523abc";
+        String s2 = "abc";
+       System.out.println(count(s1,s2));
+
+    }
+    @Test
+    /**
+     * 返回字符串s1中有多少个s2
+     */
+    public static int count(String s1, String s2) {
+        int count = 0;
+        for ( int index = 0;index <= s1.length(); ) {
+            index = s1.indexOf(s2,index);
+            if (index != -1) {
+                index++;
+                count++;
+            }else {
+                break;
+            }
+        }
+        return count;
+    }
+}
